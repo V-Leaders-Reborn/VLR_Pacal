0a6ccc9f53fd53672b185b0b169bb4d3559e6714
deliverator23 <iain.millar@gmail.com>
2020-06-22 16:56:41 +0100

    Complete 2D Art.

---
+++ VLR_Pacal/Core/Pacal_Icons_Tweak.sql
@@ -0,0 +1,16 @@
+
+UPDATE IconTextureAtlases SET Filename = 'ICON_VLR_PACAL_256.dds' WHERE Name = 'ICON_ATLAS_LEU_PACAL' and IconSize = 256;
+
+UPDATE IconTextureAtlases SET Filename = 'ICON_VLR_PACAL_80.dds' WHERE Name = 'ICON_ATLAS_LEU_PACAL' and IconSize = 80;
+
+UPDATE IconTextureAtlases SET Filename = 'ICON_VLR_PACAL_64.dds' WHERE Name = 'ICON_ATLAS_LEU_PACAL' and IconSize = 64;
+
+UPDATE IconTextureAtlases SET Filename = 'ICON_VLR_PACAL_55.dds' WHERE Name = 'ICON_ATLAS_LEU_PACAL' and IconSize = 55;
+
+UPDATE IconTextureAtlases SET Filename = 'ICON_VLR_PACAL_50.dds' WHERE Name = 'ICON_ATLAS_LEU_PACAL' and IconSize = 50;
+
+UPDATE IconTextureAtlases SET Filename = 'ICON_VLR_PACAL_48.dds' WHERE Name = 'ICON_ATLAS_LEU_PACAL' and IconSize = 48;
+
+UPDATE IconTextureAtlases SET Filename = 'ICON_VLR_PACAL_45.dds' WHERE Name = 'ICON_ATLAS_LEU_PACAL' and IconSize = 45;
+
+UPDATE IconTextureAtlases SET Filename = 'ICON_VLR_PACAL_32.dds' WHERE Name = 'ICON_ATLAS_LEU_PACAL' and IconSize = 32;
